c4fac1111ff76f20f19a42ffc33efde896edbe3c
Jlucas2010 <jlucas072010@gmail.com>
2025-01-06 19:52:29 +0000

    Add files via upload

---
+++ P 2º A 1.py
@@ -0,0 +1,13 @@
+print("Sistema de cadastro de usuarios")
+
+emailIntrodusido = input("Introdusa o seu email")
+senhaIntodusido = input()
+nome = input()
+listaUsuarios =[[n,e,s],[]]
+
+
+def emails(emailIntrodusido):
+    for x in listaUsuarios:
+        for y in listaUsuarios(x):
+            if y == emailIntrodusido:
+
