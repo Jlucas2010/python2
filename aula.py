f4c712a62cdd2bcf4e9449805cee99b5bbe0a609
Jlucas2010 <jlucas072010@gmail.com>
2025-02-24 19:17:59 +0000

    Add files via upload

--- aula.py
+++ aula.py
@@ -1,15 +1,39 @@
 
-with open("file", "r") as file:
-    c = file.read()
-    print(c)
+def criar_ficheiro():
+    nome = input("Intrudusa um nome para o ficheiro: ")
+    with open(nome, "w") as file:
+        file.write("")
+    print("Ficheiro criado.")
+    print(f"O nome do ficheiro é {nome}.")
 
-with open("file", "r") as file:
-    c1 = file.readline()
-    print(c1)
-    c2 = file.readline()
-    print(c2)
+def escrita():
+    nome = input("Nome do ficheiro a escrever: ")
+    frase = input("Intrudosa a frase: ")
+    with open(f"{nome}", "a") as file:
+        file.write(f"\n{frase}")
 
-with open("file", "r") as file:
-    c0 = file.readlines()
-    for l in c0:
-        print(l)
+def ler():
+    nome = input("Nome do ficheiro a ler: ")
+    with open(f"{nome}", "r") as file:
+        leitora = file.readlines()
+        for l in leitora:
+            print(l)
+
+def escolha():
+    print("")
+    print("Digite 1 para criar ficheiro.")
+    print("Digite 2 para escrever em um ficheiro.")
+    print("Digite 3 para ler um ficheiro.")
+    escolha = input("O que deseja fazer ? : ")
+    print("")
+    if escolha == "1":
+        criar_ficheiro()
+    elif escolha == "2":
+        escrita()
+    elif escolha == "3":
+        ler()
+
+while True:
+    escolha()
+
+
